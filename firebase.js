93f3aee8ade628e99511e0113ba8b6c8b5c8d473
Pronil Chakraborty <pronilchakraborty007@gmail.com>
2022-05-22 22:12:28 +0530

    build1.1

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEaPeehhC2QbCnciuosNOnb5AdjJECzds",
+  apiKey: API_KEY,
   authDomain: "instant-ppr.firebaseapp.com",
   projectId: "instant-ppr",
   storageBucket: "instant-ppr.appspot.com",
-  messagingSenderId: "148974641465",
-  appId: "1:148974641465:web:0ab6f33768e5e5887888e8"
+  messagingSenderId: MESSAGING_API_KEY,
+  appId: API_ID
 };
 
 // Initialize Firebase
